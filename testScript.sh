54b781279a1c183c2ab6af83ee06112df8a62115
Likutar1 <111292454+Likutar1@users.noreply.github.com>
2025-03-05 15:12:23 +0000

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,2 +1,36 @@
-#!/bin/sh
-echo "hello"
+#!/bin/bash
+
+echo "--------------------------"
+echo "Started script main.sh"
+
+websites=(
+  "https://www.salford.gov.uk abc"
+  "https://www.google.com Lucky"
+  "https://www.facebook.com Facebook"
+)
+
+for web in "${websites[@]}"; do
+  url=$(echo "$web" | awk '{print $1}')
+  keyword=$(echo "$web" | awk '{print $2}')
+
+  echo ""
+  date
+  echo "Checking $url"
+
+  response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+  echo "Response: $response"
+
+  if [[ "$response" -eq 200 ]]; then
+    echo "The site is up."
+    if curl -s "$url" | grep -q "$keyword"; then
+      echo "Keyword $keyword is found."
+    else
+      echo "Keyword $keyword was not found."
+    fi
+  elif [[ "$response" -eq 403 ]]; then
+    echo "Access refusal."
+  else
+    echo "Unsure, please check."
+  fi
+done
